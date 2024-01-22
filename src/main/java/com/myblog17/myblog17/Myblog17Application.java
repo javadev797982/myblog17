0f74ffdc756fe5500e04fb88c3d29ffc621f066c
javadev797982 <javadev797982@gmail.com>
2024-01-22 16:50:33 +0530

    143:create save post feature

---
+++ src/main/java/com/myblog17/myblog17/Myblog17Application.java
@@ -0,0 +1,13 @@
+package com.myblog17.myblog17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblog17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblog17Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/myblog17/myblog17/Myblog17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog17.myblog17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myblog17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
